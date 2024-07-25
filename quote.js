064732395161cecd4adff7446f6908314e75a1f6
Stace0x <167257706+Stace0x@users.noreply.github.com>
2024-07-25 14:17:46 +0100

    ticket 2b started but failed

--- quote.js
+++ quote.js
@@ -1,24 +1,33 @@
+import { readFile } from "node:fs";
 import fs from "node:fs/promises";
+import { get } from "node:http";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
 
-export async function getQuotes() {
-
+async function getQuotes(fileName) {
+    try {
+        const data = await readFile(fileName, 'utf8');
+        const quotes = JSON.parse(data);
+        console.log(quotes);
+    } catch (error) {
+        console.error('Error reading this file:', error)
+    }
 }
+ getQuotes('quotes.json');
 
-export async function addQuote(quoteText) {
+// export async function addQuote(quoteText) {
 
-}
+// }
 
-export async function getRandomQuote() {
+// export async function getRandomQuote() {
 
-}
+// }
 
-export async function editQuote(id, quoteText) {
+// export async function editQuote(id, quoteText) {
 
-}
+// }
 
-export async function deleteQuote(id) {
+// export async function deleteQuote(id) {
 
-}
+// }
